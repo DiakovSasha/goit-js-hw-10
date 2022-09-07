d33007c7f956e057f72f663d2cbebbc8dac52614
Diakov Sasha <diakovsasha777@gmail.com>
2022-09-07 16:37:11 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,71 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  info: document.querySelector('.country-info'),
+};
 
 const DEBOUNCE_DELAY = 300;
+const debounce = require('lodash.debounce');
+
+refs.input.addEventListener('input', debounce(onEnter, DEBOUNCE_DELAY));
+
+function onEnter(event) {
+  event.preventDefault();
+  const searchQuery = event.target.value.trim();
+  console.log(searchQuery);
+  if (searchQuery === '') {
+    clear();
+    clearCountry()
+    return;
+  }
+  fetchCountries(searchQuery)
+    .then(data => {
+      console.log(data);
+      if (data.length > 10) {
+        clear();
+        clearCountry();
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+      if (data.length === 1) {
+        clear();
+        data.map(renderCountry);
+        return;
+      }
+
+      if (data.length >= 2 && data.length <= 10) {
+        clearCountry();
+        data.map(allFindedCountries);
+      }
+    })
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
+}
+
+function renderCountry(country) {
+  const languagesEl = country.languages.map(language => language.name).join();
+  const markUp = `<img src=${country.flags.svg} class="country__img" alt=${country.name} />
+   <h1>${country.name}</h1>
+   <p> Capital: <span class='country__span'>${country.capital}</span></p>
+   <p> Popultaion: <span class='country__span'> ${country.population}</span></p>
+   <p> Languege: <span class='country__span'> ${languagesEl}</span></p>`;
+
+  refs.info.innerHTML = markUp;
+}
+function allFindedCountries(country) {
+  const markUpCoutries = `<li class = "country_item"><img src=${country.flags.svg} class="country__img" alt=${country.name} /><h2>${country.name}</h2></li>`;
+  refs.list.insertAdjacentHTML('beforeEnd', markUpCoutries);
+}
+function clear() {
+  refs.list.innerHTML = '';
+}
+function clearCountry() {
+  refs.info.innerHTML = '';
+}
